ba99a20bfa82de0ff1bbd84b452058532b60ddc7
f6 <agipreza@gmail.com>
2016-11-25 10:27:20 +0700

    add server and eslint for linter

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "react-app",
+    "installedESLint": true,
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true
+        }
+    }
+};

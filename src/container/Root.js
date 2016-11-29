d1d3732d0ec5488d13e4c4e695b9d41f3116be25
f6 <agipreza@gmail.com>
2016-11-29 15:15:21 +0700

    add material ui

--- src/container/Root.js
+++ src/container/Root.js
@@ -2,15 +2,18 @@ import React, {Component} from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
 import AsyncApp from './AsyncApp'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 const store = configureStore()
 
 export default class Root extends Component{
   render(){
     return(
-      <Provider store={store}>
-        <AsyncApp />
-      </Provider>
+      <MuiThemeProvider>
+        <Provider store={store}>
+          <AsyncApp />
+        </Provider>
+      </MuiThemeProvider>
     )
   }
 }
